543855b9c1363c10eb37498fc1052cc37239b9db
Mohammed Albayyouk <mohammedmk.albayyouk@student.uhasselt.be>
2023-04-13 15:16:17 +0200

    een navigation drawer toegevoegd maar de menu options gaat niet open bij een het kliken, het wordt nog onderzocht

--- app/src/main/java/com/example/hidaya/EventsActivity.kt
+++ app/src/main/java/com/example/hidaya/EventsActivity.kt
@@ -2,15 +2,16 @@ package com.example.hidaya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.CalendarContract.Events
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hidaya.databinding.ActivityEventsBinding
 import java.time.LocalTime
 import java.util.Date
-
+import com.google.android.material.navigation.NavigationView
 class EventsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEventsBinding
+    private lateinit var menuBarToggle: ActionBarDrawerToggle
 
     var lezing = Event("Reis door de werelden", Date(122, 11, 31), LocalTime.of(13, 0,0), 1.50)
     var iftar = Event("Iftar", Date(122, 4, 25), LocalTime.of(19, 30,0), 1.50)
@@ -26,6 +27,20 @@ class EventsActivity : AppCompatActivity() {
         var adapter = EventsAdapter(events)
         binding.eventsRecycleView.adapter = adapter
         binding.eventsRecycleView.layoutManager = LinearLayoutManager(this)
+
+        menuBarToggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.menu_open, R.string.menu_close)
+        binding.drawerLayout.addDrawerListener(menuBarToggle)
+        menuBarToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+
+            }
+            true
+        }
+
 
     }
 }
